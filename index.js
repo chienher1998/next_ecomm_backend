4bb1a5763b464eac112c3cd486e44acd9b93fa37
clement chong <clement.ch90@gmail.com>
2023-06-12 22:04:06 +0800

    Added backend post

--- index.js
+++ index.js
@@ -1,13 +1,28 @@
 import express from "express"
 import prisma from "./src/utils/prisma.js"
+import bodyParser from 'body-parser';
+
 
 const app = express()
 const port = process.env.PORT || 8080
+app.use(bodyParser.json());
 
-app.get('/', async (req, res) => {
+app.get('/users', async (req, res) => {
   const allUsers = await prisma.user.findMany()
   res.json(allUsers)
 })
+
+app.post('/users', async (req, res) => {
+  try {
+    const user = await prisma.user.create({
+      data: req.body // Assuming req.body contains the user data
+    });
+    res.send(user);// once created, the data is sent back as response in insomnia
+  } catch (error) {
+    res.status(500).send('Error creating user');
+  }
+});
+
 
 app.listen(port, () => {
   console.log(`App started; listening on port ${port}`)
