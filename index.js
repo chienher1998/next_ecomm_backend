79debb06c80228a276ba5ad0899477da39d70bf7
clement chong <clement.ch90@gmail.com>
2023-06-14 11:36:12 +0800

    .

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 // these are the backend server code
 import express from "express";
 import prisma from "./src/utils/prisma.js";
+import cors from "cors";
 import { Prisma } from "@prisma/client";
+import bcrypt from "bcryptjs";
+import { signAccessToken } from "./src/utils/jwt.js";
 
 const app = express();
 const port = process.env.PORT || 8080;
@@ -9,6 +12,11 @@ const port = process.env.PORT || 8080;
 //app.use function process or modify incoming requests before they reach the actual route handlers.
 app.use(express.json());
 //bodyParser translate the json to another format from the client request body to talk to the backend
+app.use(cors());
+
+function filter(obj, ...keys) {
+  return keys.reduce((a, c) => ({ ...a, [c]: obj[c] }), {});
+}
 
 // ----------------------Read--------------------------//
 app.get("/users", async (req, res) => {
@@ -20,18 +28,57 @@ app.get("/users", async (req, res) => {
   }
 });
 
-// -------------------Create-----------------------------//
+// -------------------CreateUser-----------------------------//
+function validateUser(input) {
+  const validationErrors = {};
+
+  if (!("username" in input) || input["username"].length == 0) {
+    validationErrors["username"] = "cannot be blank";
+  }
+
+  if (!("email" in input) || input["email"].length == 0) {
+    validationErrors["email"] = "cannot be blank";
+  }
+
+  if (!("password" in input) || input["password"].length == 0) {
+    validationErrors["password"] = "cannot be blank";
+  }
+
+  if ("password" in input && input["password"].length < 8) {
+    validationErrors["password"] = "should be at least 8 characters";
+  }
+
+  if (
+    "email" in input &&
+    !input["email"].match(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/)
+  ) {
+    validationErrors["email"] = "is invalid";
+  }
+
+  return validationErrors;
+}
+
 app.post("/users", async (req, res) => {
   const data = req.body;
+
+  const validationErrors = validateUser(data);
+
+  if (Object.keys(validationErrors).length != 0)
+    return res.status(400).send({
+      error: validationErrors,
+    });
+
+  data.password = bcrypt.hashSync(data.password, 8);
 
   prisma.user
     .create({
       data,
     })
     .then((user) => {
-      return res.json(user);
+      return res.json(filter(user, "id", "username", "email"));
     })
     .catch((err) => {
+      // we have unique index on user's email field in our schema, Postgres throws an error when we try to create 2 users with the same email. here's how we catch the error and gracefully return a friendly message to the user.
       if (
         err instanceof Prisma.PrismaClientKnownRequestError &&
         err.code === "P2002"
@@ -41,11 +88,83 @@ app.post("/users", async (req, res) => {
 
         return res.status(500).send({
           error: formattedError,
-        }); // friendly error handling
+        });
       }
-      throw err; // if this happens, our backend application will crash and not respond to the client. because we don't recognize this error yet, we don't know how to handle it in a friendly manner. we intentionally throw an error so that the error monitoring service we'll use in production will notice this error and notify us and we can then add error handling to take care of previously unforeseen errors.
+      throw err;
     });
 });
+
+// -------------------LoginUser-----------------------------//
+function validateLogin(input) {
+  const validationErrors = {};
+
+  if (!("email" in input) || input["email"].length == 0) {
+    validationErrors["email"] = "cannot be blank";
+  }
+
+  if (!("password" in input) || input["password"].length == 0) {
+    validationErrors["password"] = "cannot be blank";
+  }
+
+  if (
+    "email" in input &&
+    !input["email"].match(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/)
+  ) {
+    validationErrors["email"] = "is invalid";
+  }
+
+  return validationErrors;
+}
+
+app.post("/sign-in", async (req, res) => {
+  const data = req.body;
+
+  const validationErrors = validateLogin(data);
+
+  if (Object.keys(validationErrors).length != 0)
+    return res.status(400).send({
+      error: validationErrors,
+    });
+
+  const user = await prisma.user.findUnique({
+    where: {
+      email: data.email,
+    },
+  });
+
+  if (!user)
+    return res.status(401).send({
+      error: "Email address or password not valid",
+    });
+
+  const checkPassword = bcrypt.compareSync(data.password, user.password);
+  if (!checkPassword)
+    return res.status(401).send({
+      error: "Email address or password not valid",
+    });
+
+  const accessToken = await signAccessToken(user);
+  return res.json({ accessToken });
+});
+
+
+
+// app.delete("/users", async (req, res) => {
+//   const data = req.body;
+//     prisma.user.delete({
+//       where: {
+//         data,
+//       },
+//     })
+//     .then(()=>{
+//       return res.json("User deleted");
+//     })
+//     .catch((err) => {
+//     return res
+//       .status(500)
+//       .json({ error: "An error occurred while deleting the user" });
+//   })
+// });
 
 //used to start a web server and listen for incoming HTTP requests on a specific port
 app.listen(port, () => {
--- temp.js
+++ temp.js
@@ -1,20 +1,36 @@
+// async function createUser() {
+//     const resp = await fetch('http://localhost:8080/users', {
+//         method: 'POST',
+//         headers: {
+//             'Content-type': 'application/json'
+//         },
+//         body: JSON.stringify({
+//             name: 'test',
+//             email: 'something2@example.com',
+//             password: 'secure'
+//         })
+//     })
 
-async function createUser() {
-    const resp = await fetch('http://localhost:8080/users', {
-        method: 'POST',
-        headers: {
-            'Content-type': 'application/json'
-        },
-        body: JSON.stringify({
-            name: 'test',
-            email: 'something2@example.com',
-            password: 'secure'
-        })
-    })
+//     const res = await resp.json()
 
-    const res = await resp.json()
+//     console.log({ res })
+// }
 
-    console.log({ res })
-}
+// import prisma from "./src/utils/prisma.js";
 
+// async function main() {
+//   prisma.user
+//     .delete({
+//       where: {
+//         email: "123@testing.com",
+//       },
+//     })
+//     .then((user) => {
+//       console.log(user);
+//     })
+//     .catch((e) => {
+//       console.log(e.message);
+//     });
+// }
 
+// main();
